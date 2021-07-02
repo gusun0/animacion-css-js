69696ecedd3521ec51c30316421c5bd40f55e6ba
gusun0 <abr.alfaros@outlook.com>
2021-07-02 14:36:52 -0500

    animacion

---
+++ app.js
@@ -0,0 +1,47 @@
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+// items
+const title = document.querySelector('.title');
+const logo = document.querySelector('.logo img');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
+
+	
+
+// moving animation event
+container.addEventListener('mousemove', (e) => {
+	let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+	let yAxis = (window.innerHeight / 2 - e.pageY) / 10;	
+	card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+	
+});
+
+
+// animate in
+container.addEventListener('mouseenter', (e) => {
+	card.style.transition = 'none';
+	// pop out
+	title.style.transform = 'translateZ(150px)';
+	logo.style.transform = 'translateZ(200px) rotateZ(-45deg)';
+	description.style.transform = 'translateZ(125px)';
+	sizes.style.transform = 'translateZ(100px)';
+	purchase.style.transform = 'translateZ(75px)';
+});
+
+
+
+
+// animate out
+container.addEventListener('mouseleave', (e) => {
+	card.style.transition = 'all 0.5s ease';
+	card.style.transform = `rotateY(0deg) rotateX(0deg)`;	
+	// popback
+	title.style.transform = "translateZ(0px)";
+	logo.style.transform = 'translateZ(0px) rotateZ(0deg)';
+	description.style.transform = 'translateZ(0px)';
+	sizes.style.transform = 'translateZ(0px)';
+	purchase.style.transform = 'translateZ(0px)';
+});
